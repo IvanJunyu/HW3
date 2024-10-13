35e5e0745aad5619e40da9f9c6d2fc825c16da2e
Ivan Loh <ivanjloh2020@gmail.com>
2024-10-13 19:53:36 -0400

    finish hw

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,63 +8,197 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Card Matching Game',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 50, 119, 248)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Counter Button'),
+      home: const GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GameScreenState createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Push the button to increase counter:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return ChangeNotifierProvider(
+      create: (_) => GameProvider(vsync: this),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Matching Game'),
+        ),
+        body: Center(
+          child: Consumer<GameProvider>(
+            builder: (context, gameProvider, child) {
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, 
+                  childAspectRatio: 1,
+                ),
+                itemCount: gameProvider.cards.length,
+                itemBuilder: (context, index) {
+                  return CardWidget(index: index);
+                },
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class CardWidget extends StatefulWidget {
+  final int index;
+
+  const CardWidget({super.key, required this.index});
+
+  @override
+  _CardWidgetState createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final gameProvider = Provider.of<GameProvider>(context);
+    final card = gameProvider.cards[widget.index];
+
+    return GestureDetector(
+      onTap: () {
+        if (!gameProvider.isAnimating) {
+          gameProvider.flipCard(widget.index);
+        }
+      },
+      child: AnimatedBuilder(
+        animation: card.controller,
+        builder: (context, child) {
+          final isFlipped = card.controller.value >= 0.5;
+
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationY(card.controller.value * 3.5) 
+              ..scale(1 + (0.1 * card.controller.value)), 
+            child: isFlipped ? _buildFrontCard(card.frontImage) : _buildBackCard(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildFrontCard(String imagePath) {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackCard() {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+        image: const DecorationImage(
+          image: AssetImage('assets/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class CardModel {
+  final String frontImage;
+  bool isFaceUp;
+  late AnimationController controller; 
+
+  CardModel({required this.frontImage, this.isFaceUp = false, required TickerProvider vsync}) {
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 800), 
+      vsync: vsync,
+    );
+  }
+}
+
+class GameProvider with ChangeNotifier {
+  List<CardModel> cards = [];
+  bool isAnimating = false;
+  List<int> flippedCards = [];
+  final TickerProvider vsync;
+
+  GameProvider({required this.vsync}) {
+    _initializeCards();
+  }
+
+  void _initializeCards() {
+    List<String> images = [
+      'assets/1.png',
+      'assets/2.png',
+      'assets/3.png',
+      'assets/4.png',
+      'assets/5.png',
+      'assets/6.png',
+      'assets/7.png',
+      'assets/8.png',
+      'assets/9.png',
+      'assets/10.png',
+      'assets/11.png',
+      'assets/12.png',
+    ];
+
+    cards = images
+        .expand((image) => [
+              CardModel(frontImage: image, vsync: vsync),
+              CardModel(frontImage: image, vsync: vsync)
+            ])
+        .toList();
+
+    cards.shuffle();
+    notifyListeners();
+  }
+
+  void flipCard(int index) {
+    if (flippedCards.length < 2 && !cards[index].isFaceUp && !isAnimating) {
+      cards[index].isFaceUp = true;
+      cards[index].controller.forward();
+      flippedCards.add(index);
+
+      if (flippedCards.length == 2) {
+        isAnimating = true;
+        _checkMatch();
+      }
+      notifyListeners();
+    }
+  }
+
+  void _checkMatch() {
+    Future.delayed(const Duration(seconds: 1), () {
+      if (cards[flippedCards[0]].frontImage == cards[flippedCards[1]].frontImage) {
+        flippedCards.clear();
+      } else {
+        cards[flippedCards[0]].isFaceUp = false;
+        cards[flippedCards[1]].isFaceUp = false;
+        cards[flippedCards[0]].controller.reverse();
+        cards[flippedCards[1]].controller.reverse();
+        flippedCards.clear();
+        notifyListeners();
+      }
+      isAnimating = false;
+    });
   }
 }
